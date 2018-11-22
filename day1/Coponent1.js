45dec40747aedc3bc7a0c109537a5f195588d58d
santosh <santosh.ece06@gmail.com>
2018-11-22 12:59:32 +0530

    Compoente 1

---
+++ day1/Coponent1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Component1 extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>Hello Native</Text>
+            </View>
+        );
+    }
+}
+
