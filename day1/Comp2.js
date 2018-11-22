78e58d7e81dcb4ea3a08fa78ca6e54e97384514f
santosh <santosh.ece06@gmail.com>
2018-11-22 13:47:47 +0530

    inline style component 2.

--- day1/Comp2.js
+++ day1/Comp2.js
@@ -4,8 +4,8 @@ import {View, Text} from 'react-native';
 export class Comp2 extends React.Component{
     render(){
         return (
-            <View>
-                <Text>Hello Component!!!</Text>
+            <View style={{backgroundColor:'royalblue'}}>
+                <Text style={{color:'white', fontSize:25}}>Hello Component!!!</Text>
             </View>
         );
     }
