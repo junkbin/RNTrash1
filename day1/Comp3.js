ec610831611dd41146f4f2b465506058fb97c1d8
santosh <santosh.ece06@gmail.com>
2018-11-22 14:26:44 +0530

    component 3.

---
+++ day1/Comp3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export class Comp3 extends React.Component {
+    render(){
+        return (
+            <View style={{backgroundColor:'royalblue', justifyContent:'center', 'flex':1, alignItems:'center'}}>
+                <Text style={{color:'white', fontSize:40}}>
+                    Hariharrr Affixus
+                </Text>
+            </View>
+        );
+    }
+}
