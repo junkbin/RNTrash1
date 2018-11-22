a9623b3f9b43e7f9da33b8fc1be298bc3ddce155
santosh <santosh.ece06@gmail.com>
2018-11-22 13:31:15 +0530

    Compoent 2

---
+++ day1/Comp2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export class Comp2 extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>Hello Component!!!</Text>
+            </View>
+        );
+    }
+}
